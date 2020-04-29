4fd2badc7b41448e596fce8a96965c21b2330805
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-04-28 21:53:19 -0500

    Implement backpack methods

--- src/main/scala/algoritmogenetico/Backpack.scala
+++ src/main/scala/algoritmogenetico/Backpack.scala
@@ -1,15 +1,27 @@
 package algoritmogenetico
 
-class Backpack(val indviduals: List[Seq[Int]]) {
-  def caloriesFitness: Double = {
-    1.0
+class Backpack(val items: Seq[Int], var calF: Double = 0, var weiF: Double = 0) {
+  def caloriesFitness(totalCalories: Double): Double = {
+    calF = DataTable.getTotalCalories(items) / totalCalories
+    calF
   }
 
-  def weightFitness: Double = {
-    1.0
+  def weightFitness(totalWeight: Double): Double = {
+    weiF = DataTable.getTotalWeight(items) / totalWeight
+    weiF
   }
 
-  def totalFitness: Double = {
-    2.0
+  def totalFitness(totalCalories: Double, totalWeight: Double): Double = {
+    caloriesFitness(totalCalories) + weightFitness(totalWeight)
+  }
+
+  override def toString: String = {
+    f"""
+       |$items
+       |weightFitness: $weiF
+       |caloriesFitness: $calF
+       |calories: ${DataTable.getTotalCalories(items)}
+       |weight: ${DataTable.getTotalWeight(items)}
+       |"""".stripMargin
   }
 }
