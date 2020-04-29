6584bfbcfe5f92515292d050d8dd4f657169f1d1
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-04-29 01:47:28 -0500

    Implement GA loop

--- src/main/scala/algoritmogenetico/Main.scala
+++ src/main/scala/algoritmogenetico/Main.scala
@@ -7,9 +7,28 @@ import scala.util.Random
  *
  */
 object Main extends App {
-  val initialPopulation = new Population(Population.createPopulation(4, 8))
-  println(f"Initial population: $initialPopulation")
+  var population = new Population(Population.createPopulation(4, 8))
+  println(f"Initial population: $population")
 
-  var currentBest = initialPopulation.getBest
+  var currentBest = population.getBest
   print(currentBest)
+
+  var p = 0
+  val limit = 50
+  while (p < limit) {
+    println(f"##### ${p+1}th iteration #####")
+
+    val newBest = population.getBest
+
+    // TODO: Implement comparison function, total fitness is used temporarily
+    if (currentBest.totalF < newBest.totalF) {
+      currentBest = newBest
+    }
+
+    population = Population.newPopulationByCrossing(population.population, Random.between(1, 7))
+
+    p += 1
+  }
+
+  println(f"The best individual is $currentBest")
 }
