bcf3c8b47332112b6135902e7eb7c7a1af23138a
Sergio Saucedo <sergio.saucedo10@gmail.com>
2020-04-30 03:50:00 -0500

    Generate backpack fitness at constructor, improve getBest method

--- src/main/scala/algoritmogenetico/Population.scala
+++ src/main/scala/algoritmogenetico/Population.scala
@@ -4,6 +4,13 @@ import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
 class Population(val population: List[Backpack]) {
+  population.foreach(_.totalFitness(getCalories, getWeight))
+
+  /**
+   * Returns the total calories of the population
+   *
+   * @return
+   */
   def getCalories: Double = {
     var calories = 0.0
     for (individual <- population) {
@@ -12,6 +19,11 @@ class Population(val population: List[Backpack]) {
     calories
   }
 
+  /**
+   * Returns the total weight of the population
+   *
+   * @return
+   */
   def getWeight: Double = {
     var weight = 0.0
     for (individual <- population) {
@@ -20,22 +32,40 @@ class Population(val population: List[Backpack]) {
     weight
   }
 
-  def getBest: Backpack = {
+  /**
+   * Returns the backpack that meet the constraints and has the highest calories, if it does not meet the constraints
+   * returns the backpack with the highest calories
+   *
+   * @param maxWeight   maximum weight allowed
+   * @param minCalories minimum calories allowed
+   * @return
+   */
+  def getBest(maxWeight: Double, minCalories: Double): Option[Backpack] = {
+    var afterConstraints = ListBuffer[Backpack]()
+
+    population.foreach(backpack => {
+      if (DataTable.getTotalWeight(backpack.items) < maxWeight
+        && DataTable.getTotalCalories(backpack.items) > minCalories) afterConstraints += backpack
+    })
     var bestIndex = 0
-    var bestFitness = 0.0
-    population.zipWithIndex.foreach { case (backpack, index) =>
-      val fitness = backpack.totalFitness(getCalories, getWeight)
-      if (fitness > bestFitness) {
-        bestFitness = fitness
-        bestIndex = index
+    var mostCalories = 0.0
+    var result: Option[Backpack] = None
+    if (afterConstraints.toList.nonEmpty) {
+      afterConstraints.toList.zipWithIndex.foreach { case (backpack, index) =>
+        val calories: Double = DataTable.getTotalCalories(backpack.items)
+        if (calories > mostCalories) {
+          bestIndex = index
+          mostCalories = calories
+        }
       }
+      result = Some(afterConstraints(bestIndex))
     }
-    population(bestIndex)
+    result
   }
 
   override def toString: String = {
     var str = ""
-    population.zipWithIndex.foreach( { case(item, index) =>
+    population.zipWithIndex.foreach({ case (item, index) =>
       str += f"\n#### Individual ${index + 1} ####${item.toString}"
     })
     str
@@ -52,7 +82,9 @@ object Population {
   def createPopulation(individuals: Int, chromosomes: Int): List[Backpack] = {
     val population = new ListBuffer[Backpack]()
     for (individual <- 1 to individuals) {
-      population += new Backpack(Seq.fill(chromosomes) { Random.between(0, 2) })
+      population += new Backpack(Seq.fill(chromosomes) {
+        Random.between(0, 2)
+      })
     }
     population.toList
   }
@@ -61,7 +93,7 @@ object Population {
     val sortedByWeighing = population.sortBy(_.totalF)
     var newPopulation = new ListBuffer[Backpack]()
     var start = 0
-    while (start < population.length ) {
+    while (start < population.length) {
       newPopulation += new Backpack(sortedByWeighing(start).items.take(crossing) ++ sortedByWeighing(start + 1).items.drop(crossing))
       newPopulation += new Backpack(sortedByWeighing(start).items.drop(crossing) ++ sortedByWeighing(start + 1).items.take(crossing))
       start += 2
